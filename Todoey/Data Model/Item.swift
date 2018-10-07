707f8993628ecf9d3edbde1e2beff9be6c4af000
Jan Sandoval <sandovaljan@yahoo.es>
2018-10-07 17:00:59 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jan Sandoval on 10/7/18.
+//  Copyright © 2018 Jan Sandoval. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
